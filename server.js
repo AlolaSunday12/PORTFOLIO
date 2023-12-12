7b3291cb2a68c133ebd79f2cddc65dab2c40bad7
Alola Sunday joseph <122814299+AlolaSunday12@users.noreply.github.com>
2023-12-12 20:39:52 +0100

    update

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 const express = require("express");
-const bookingRoute = require('./routes/bookingsRoute');
-
 const app = express();
-
-const dbconfig = require('./db')
-app.use(express.json());
-const roomsRoute = require('./routes/roomsRoute')
-const usersRoute = require('./routes/usersRoute')
-const bookingsRoute = require('./routes/bookingsRoute')
-app.use(express.json())
-
-app.use(`/api/rooms` , roomsRoute)
-app.use('/api/users' , usersRoute)
-app.use('/api/bookings' , bookingRoute)
 const port = process.env.PORT || 5001;
 
-app.listen(port, () => console.log('node server started using nodemon'));
+// Middleware to set Content Security Policy header
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'none'; font-src *; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval';"
+  );
+  next();
+});
+
+// Other Middleware
+app.use(express.json());
+
+// Routes
+const roomsRoute = require('./routes/roomsRoute');
+const usersRoute = require('./routes/usersRoute');
+const bookingRoute = require('./routes/bookingsRoute');
+app.use('/api/rooms', roomsRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/bookings', bookingRoute);
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Hello, this is the root route!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Node server started on port ${port} using nodemon`);
+});
