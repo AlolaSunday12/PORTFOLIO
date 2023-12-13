1798c436c528911f5e6ac37b68fbd3d86e59cba1
Alola Sunday joseph <122814299+AlolaSunday12@users.noreply.github.com>
2023-12-13 02:16:11 +0100

    update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const roomsRoute = require('./routes/roomsRoute')
 const usersRoute = require('./routes/usersRoute')
 const bookingsRoute = require('./routes/bookingsRoute')
-
+app.use(express());
 app.use(`/api/rooms` , roomsRoute)
 app.use('/api/users' , usersRoute)
 app.use('/api/bookings' , bookingRoute)
