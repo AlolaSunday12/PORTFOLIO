de3f72966745787ccd24003bb8b9a3d2b82c4736
Alola Sunday joseph <122814299+AlolaSunday12@users.noreply.github.com>
2023-12-13 05:00:23 +0100

    update

--- server.js
+++ server.js
@@ -1,32 +1,17 @@
 const express = require("express");
-const helmet = require("helmet");
 const bookingRoute = require('./routes/bookingsRoute');
-const dbconfig = require('./db');
 
 const app = express();
 
-// Use helmet middleware for security headers, including CSP
-app.use(helmet());
-
-// Set up Content Security Policy (CSP) to allow fonts
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'"],
-    styleSrc: ["'self'"],
-    fontSrc: ["'self'", 'https://fonts.gstatic.com'], // Add the font source URL(s)
-  },
-}));
-
-// Other middleware and route setup
+const dbconfig = require('./db')
 app.use(express.json());
-const roomsRoute = require('./routes/roomsRoute');
-const usersRoute = require('./routes/usersRoute');
-const bookingsRoute = require('./routes/bookingsRoute');
-app.use('/api/rooms', roomsRoute);
-app.use('/api/users', usersRoute);
-app.use('/api/bookings', bookingRoute);
-
+const roomsRoute = require('./routes/roomsRoute')
+const usersRoute = require('./routes/usersRoute')
+const bookingsRoute = require('./routes/bookingsRoute')
+app.use(express());
+app.use(`/api/rooms` , roomsRoute)
+app.use('/api/users' , usersRoute)
+app.use('/api/bookings' , bookingRoute)
 const port = process.env.PORT || 5001;
 
-app.listen(port, () => console.log('Node server started using nodemon'));
+app.listen(port, () => console.log('node server started using nodemon'));
