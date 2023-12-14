e4dee06779a7e213d27b5cc35fd92a5372d4c63d
Alola Sunday joseph <122814299+AlolaSunday12@users.noreply.github.com>
2023-12-14 13:59:21 +0100

    update

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const roomsRoute = require('./routes/roomsRoute');
 const usersRoute = require('./routes/usersRoute');
 app.use(`/api/rooms`, roomsRoute);
 app.use('/api/users', usersRoute);
-app.use('/api/bookings', bookingRoute);  // Use the consistent name
+app.use('/api/bookings', bookingRoute);
 
 const port = process.env.PORT || 5001;
 
