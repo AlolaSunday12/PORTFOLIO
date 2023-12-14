141f3a0f903519adb0526ca85f8515ef83da51f5
Alola Sunday joseph <122814299+AlolaSunday12@users.noreply.github.com>
2023-12-14 13:54:33 +0100

    update

--- server.js
+++ server.js
@@ -22,50 +22,13 @@ app.use((req, res, next) => {
   next();
 });
 
-const dbconfig = require('./db')
+const dbconfig = require('./db');
 app.use(express.json());
-const roomsRoute = require('./routes/roomsRoute')
-const usersRoute = require('./routes/usersRoute')
-const bookingsRoute = require('./routes/bookingsRoute')
-app.use(`/api/rooms`, roomsRoute)
-app.use('/api/users', usersRoute)
-app.use('/api/bookings', bookingRoute)
-
-const port = process.env.PORT || 5001;
-
-app.listen(port, () => console.log('node server started using nodemon'));
- require("express");
-const bookingRoute = require('./routes/bookingsRoute');
-
-const app = express();
-
-// Set up Content Security Policy middleware for local development
-app.use((req, res, next) => {
-  res.setHeader('Content-Security-Policy', `
-    default-src 'self';
-    script-src 'self' http://localhost:3000;  // Adjust the port if needed
-    style-src 'self' http://localhost:3000;   // Adjust the port if needed
-    font-src 'self' data: https://cdnjs.cloudflare.com;
-    img-src 'self' data: http://localhost:3000;
-    manifest-src 'self';
-  `);
-
-  // Additional headers for security
-  res.setHeader('X-Content-Type-Options', 'nosniff');
-  res.setHeader('X-Frame-Options', 'deny');
-  res.setHeader('Referrer-Policy', 'no-referrer');
-
-  next();
-});
-
-const dbconfig = require('./db')
-app.use(express.json());
-const roomsRoute = require('./routes/roomsRoute')
-const usersRoute = require('./routes/usersRoute')
-const bookingsRoute = require('./routes/bookingsRoute')
-app.use(`/api/rooms`, roomsRoute)
-app.use('/api/users', usersRoute)
-app.use('/api/bookings', bookingRoute)
+const roomsRoute = require('./routes/roomsRoute');
+const usersRoute = require('./routes/usersRoute');
+app.use(`/api/rooms`, roomsRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/bookings', bookingRoute);  // Use the consistent name
 
 const port = process.env.PORT || 5001;
 
