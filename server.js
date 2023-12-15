fca7a781a76058fdf49e1ca7b50cbb47a4b26d22
Alola Sunday joseph <122814299+AlolaSunday12@users.noreply.github.com>
2023-12-15 21:28:21 +0100

    update

--- server.js
+++ server.js
@@ -7,20 +7,7 @@ app.use(cors());
 
 // Set up Content Security Policy middleware for local development
 app.use((req, res, next) => {
-  res.setHeader('Content-Security-Policy', `
-    default-src 'self';
-    script-src 'self' http://localhost:3000;  // Adjust the port if needed
-    style-src 'self' http://localhost:3000;   // Adjust the port if needed
-    font-src 'self' data: https://cdnjs.cloudflare.com;
-    img-src 'self' data: http://localhost:3000;
-    manifest-src 'self';
-  `);
-
-  // Additional headers for security
-  res.setHeader('X-Content-Type-Options', 'nosniff');
-  res.setHeader('X-Frame-Options', 'deny');
-  res.setHeader('Referrer-Policy', 'no-referrer');
-
+  res.setHeader('Content-Security-Policy', "default-src 'self';");
   next();
 });
 
